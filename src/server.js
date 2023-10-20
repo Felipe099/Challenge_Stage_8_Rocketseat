223c9280a66bdbd67a4ac834cbbae45b0e5b6e5c
Felipe099 <lipepagodart99@gmail.com>
2023-10-20 16:53:06 -0300

    add dontenv and pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+require('dotenv/config');
 
 const express = require('express');
 const migrationsRun = require('./database/sqlite/migrations');
@@ -20,7 +21,7 @@ app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
-app.use((error, requeste, response, next) => {
+app.use((error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             status: 'error',
@@ -36,7 +37,7 @@ app.use((error, requeste, response, next) => {
     });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3332;
 
 app.listen(PORT, () => {
     PORT, console.log(`Project Running on ${PORT}`);
